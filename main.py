8d79d5e95b6ef5224bf8acd1357357629b948578
OgnjenR <ognjenraketic23@outlook.com>
2022-09-05 00:17:56 +0200

    Another Exercise with Calculator

---
+++ main.py
@@ -0,0 +1,10 @@
+#Task is to calculate remain of our life in days, weeks and months
+
+age = input("What is your current age?")
+
+years_remaining = 90 - int(age)
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining *12
+
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left")
